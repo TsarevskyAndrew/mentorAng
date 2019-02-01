bbf19561df100c54ef518e2a569dde1567c0e384
DmitryZhezhera <zhezhera.d@mail.ru>
2019-02-01 11:32:02 +0200

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/header/header.component';
+import { FooterComponent } from './modules/footer/footer.component';
+import { MainComponent } from './modules/main/main.component';
+import { CourseComponent } from './pages/course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
